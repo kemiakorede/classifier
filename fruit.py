d4c86d1a76eab62ccbad787faeddb92d102d1f6b
kemiakorede <66761223+kemiakorede@users.noreply.github.com>
2023-09-03 22:23:45 +0100

    Add files via upload

---
+++ fruit.py
@@ -0,0 +1,53 @@
+import streamlit as st
+import tensorflow as tf
+from PIL import Image
+import numpy as np
+
+# Constants
+IMAGE_SIZE = 160
+MODEL_PATH = "MobileNetV2 (1).h5"
+
+# Hard-coded list of class names
+class_names = [
+    'apple', 'banana', 'beetroot', 'bell pepper', 'cabbage', 'capsicum',
+    'carrot', 'cauliflower', 'chilli pepper', 'corn', 'cucumber', 'eggplant',
+    'garlic', 'ginger', 'grapes', 'jalepeno', 'kiwi', 'lemon', 'lettuce',
+    'mango', 'onion', 'orange', 'paprika', 'pear', 'peas', 'pineapple',
+    'pomegranate', 'potato', 'raddish', 'soy beans', 'spinach', 'sweetcorn',
+    'sweetpotato', 'tomato', 'turnip', 'watermelon'
+]
+
+@st.cache(allow_output_mutation=True)
+def load_model():
+    model = tf.keras.models.load_model(MODEL_PATH, compile=False)
+    return model
+
+def preprocess_image(image: Image.Image) -> np.ndarray:
+    image = image.resize((IMAGE_SIZE, IMAGE_SIZE))
+    image = np.array(image) / 255.0
+    image = image.astype(np.float32)
+    image = np.expand_dims(image, axis=0)
+    return image
+
+# Load the model
+model = load_model()
+
+# Streamlit UI code
+st.title("Fruit and Vegetable Classifier")
+st.write("""
+Upload an image of a fruit or vegetable and the model will predict its name.
+""")
+
+uploaded_image = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+
+if uploaded_image is not None:
+    image = Image.open(uploaded_image)
+    st.image(image, caption="Uploaded Image.", use_column_width=True)
+    st.write("")
+    st.write("Classifying...")
+    
+    processed_image = preprocess_image(image)
+    predictions = model.predict(processed_image)
+    predicted_class = np.argmax(predictions)
+    
+    st.write(f"The model predicts this item as: {class_names[predicted_class]}")
